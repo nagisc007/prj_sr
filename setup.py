8c9a1e15a20ed2ab8bed0e765aff4cca2990d6f0
nagisc007 <nagisc007@yahoo.co.jp>
2019-04-11 13:59:58 +0900

    Added: setup py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+import sys
+
+sys.path.append('./storybuilder/builder')
+sys.path.append('./src')
+sys.path.append('./tests')
+
+setup(
+        name = 'The SR project',
+        version = '0.0.1',
+        description = "This is a novel project of the SR",
+        packages = find_packages(),
+        test_suite = 'test_all.suite'
+)
