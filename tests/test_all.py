b53c815fa0ca67bb320cda7db7ca7175f1dedd44
nagisc007 <nagisc007@yahoo.co.jp>
2019-04-11 14:24:28 +0900

    Added: test files

---
+++ tests/test_all.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""Test suite for all tests.
+"""
+import unittest
+
+
+def suite():
+    '''Packing all tests.
+
+    Returns:
+        obj:`TestSuite`: testing suite object contained test cases.
+    '''
+    suite = unittest.TestSuite()
+
+    return suite
