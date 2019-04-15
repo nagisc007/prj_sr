6fff04626bc9d97771c0aace123e91747826b146
nagisc007 <nagisc007@yahoo.co.jp>
2019-04-15 21:24:04 +0900

    Added #4: chapter08 outline

--- src/chapter08.py
+++ src/chapter08.py
@@ -14,11 +14,23 @@ from storybuilder.builder.tools import build_to_story
 # main
 def story08(ma: Master):
     return ma.story("Race 8",
+            ma.anri.have(ma.grandticket),
+            ma.anri.be(ma.factory, ma.race_consday.elapsed_day(day=10)),
+            ma.factory.be("廃業"),
+            ma.anri.talk(ma.chief, "別れ"),
+            ma.anri.go(ma.grandracestage, ma.grandprixday),
+            ma.anri.be(ma.angelwing, "メンテ不足"),
+            ma.anri.have(ma.grandrace, "優勝"),
+            ma.anri.look(ma.dad),
+            ma.anri.go(ma.grandrace, "開幕"),
+            ma.anri.look(ma.ren),
+            ma.anri.have("奪う", "他人のパーツ"),
+            ma.anri.have(ma.bugparts),
             )
 
 def main(): # pragma: no cover
     from main import master
-    return build_to_story(story(master()))
+    return build_to_story(story08(master()))
 
 
 if __name__ == '__main__':
