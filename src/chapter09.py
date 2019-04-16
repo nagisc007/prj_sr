edd66759b1d89a5102c72b8dfdb88628b49ff9a6
nagisc007 <nagisc007@yahoo.co.jp>
2019-04-16 09:34:23 +0900

    Added #4: chapter09 outline

--- src/chapter09.py
+++ src/chapter09.py
@@ -14,11 +14,23 @@ from storybuilder.builder.tools import build_to_story
 # main
 def story09(ma: Master):
     return ma.story("Race 9",
+            ma.anri.be(ma.grandrace, ma.grandracestage, ma.grandprixday),
+            ma.anri.have(ma.grandrace, "優勝").want(),
+            ma.anri.ask(ma.dad, ma.dad_reason),
+            ma.ren.talk(ma.anri, ma.dad),
+            ma.anri.reply(ma.ren, "知っているの？"),
+            ma.ren.talk(ma.anri, "勝ったら教えてあげる"),
+            ma.ren.do(ma.angelwing, "壊す"),
+            ma.ren.go("先に行ってしまう"),
+            ma.anri.go().non(),
+            ma.anri.think("優勝したい理由"),
+            ma.anri.do(ma.ren, "勝利"),
+            ma.anri.know(ma.ren, ma.ren_secret),
             )
 
 def main(): # pragma: no cover
     from main import master
-    return build_to_story(story(master()))
+    return build_to_story(story09(master()))
 
 
 if __name__ == '__main__':
