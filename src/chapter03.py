0c7163e76028b0b0ca05070c645a92f5e77a7a66
N.T.WORKS <nagisc007@yahoo.co.jp>
2019-04-18 17:16:20 +0900

    [WIP] Added #7: chapter3 episode test (#34)
    * [WIP] Added: chapter3 episode test
    
    * [WIP] Added: chapter 3 ep4
    
    * [WIP] Added: ep3
    
    * Added: chapter3 episode1
    
    * Fixed: pragma
    
    resolved #7

--- src/chapter03.py
+++ src/chapter03.py
@@ -10,20 +10,76 @@ from storybuilder.builder.tools import build_to_story
 
 
 # episodes
+def ep_intro(m: Master):
+    racetime = m.race1day.elapsed_day(hour=11, min=10)
+    return m.story("冒頭",
+            m.comment("レース開始の振り返りと簡単な説明"),
+            m.anri.be("出遅れた", m.race1stage, racetime),
+            m.anri.be("スタートできていない"),
+            m.anri.know("何が起きたか").must(),
+            m.anri.remember("スタート"),
+            m.anri.think(m.yabu, "嵌められた"),
+            m.anri.go("急ぐ"),
+            m.anri.look("自分は最後尾"),
+            )
 
-# main
-def story03(ma: Master):
-    return ma.story("Race 3",
-            ma.anri.be("出遅れた", ma.race1stage, ma.race1day),
-            ma.anri.have(ma.race1, "優勝").want(),
-            ma.anri.have(ma.grandticket).can(),
-            ma.taiga.do("皆殺し"),
-            ma.anri.have(ma.yabu, "協力"),
-            ma.anri.do(ma.taiga, "倒す", ma.yabu, "協力して"),
-            ma.angelwing.be("壊れる"),
-            ma.anri.be("失速"),
-            ma.yabu.have(ma.race1, "優勝"),
-            ma.anri.have("優勝を逃す"),
+def ep_dashdash(m: Master):
+    racetime = m.race1day.elapsed_day(hour=11, min=15)
+    course = m.race1stage.insided("市街地コース")
+    return m.story("追いつけ！",
+            m.anri.go(course, racetime),
+            course.explain("壊れたビルの残骸"),
+            m.anri.deal("遅れを取り戻す").must(),
+            m.anri.be("出遅れた"),
+            m.anri.do(m.angelwing, "使う"),
+            m.angelwing.explain("母の遺品", "全身が壊れそうなほどの速度"),
+            m.anri.go(m.yabu, "追いつく"),
+            )
+
+def ep_recoop(m: Master):
+    racetime = m.race1day.elapsed_day(hour=11, min=30)
+    course = m.race1stage.insided("ビルの谷間")
+    lastzone = m.race1stage.insided("ゴール前")
+    return m.story("ゴール直前の壁",
+            m.anri.go(m.race1, "ゴール"),
+            m.anri.have(m.race1, "優勝").want(),
+            m.anri.be(racetime, course),
+            m.anri.ask(m.yabu, m.yabu_reason),
+            m.yabu.reply(m.anri, m.yabu_reason),
+            m.yabu.talk(m.anri, "どんな手を使っても勝つ", "これがSR"),
+            m.anri.ask(m.yabu, "それでもずっと勝ててない"),
+            m.yabu.talk(m.anri, m.taiga, "あいつがいる"),
+            m.anri.go("先に進む"),
+            m.anri.go(lastzone),
+            m.taiga.do("皆殺し"),
+            m.anri.be(m.taiga, "立ち塞がる"),
+            )
+
+def ep_lostwin(m: Master):
+    racetime = m.race1day.elapsed_day(hour=11, min=45)
+    lastzone = m.race1stage.insided("ゴール前")
+    return m.story("逃げる優勝",
+            m.anri.be(lastzone, racetime),
+            m.anri.go("ゴール", m.taiga).non(),
+            m.anri.have(m.race1, "優勝").want(),
+            m.anri.have(m.grandticket).must(),
+            m.anri.have(m.yabu, "協力"),
+            m.anri.do(m.taiga, "倒す", m.yabu, "協力"),
+            m.anri.be("壊れる"),
+            m.yabu.be("壊れる"),
+            m.anri.be("失速"),
+            m.yabu.be("失速"),
+            m.yabu.have(m.race1, "優勝"),
+            m.anri.have(m.race1, "優勝").non(),
+            )
+
+# m.n
+def story03(m: Master):
+    return m.story("Race 3",
+            ep_intro(m),
+            ep_dashdash(m),
+            ep_recoop(m),
+            ep_lostwin(m),
             )
 
 def main(): # pragma: no cover
--- tests/test_chap03.py
+++ tests/test_chap03.py
@@ -6,7 +6,7 @@ import storybuilder.builder.testtools as testtools
 from storybuilder.builder.sbutils import print_test_title
 from src.main import master, story
 from src.main import CHARAS, STAGES
-from src.chapter03 import story03
+from src.chapter03 import story03, ep_intro, ep_dashdash, ep_recoop, ep_lostwin
 
 
 _FILENAME = 'chapter03.py'
@@ -21,6 +21,10 @@ class StoryTest(unittest.TestCase):
     def setUp(self):
         self.ma = master()
         self.story = story03(self.ma)
+        self.ep1 = ep_intro(self.ma)
+        self.ep2 = ep_dashdash(self.ma)
+        self.ep3 = ep_recoop(self.ma)
+        self.ep4 = ep_lostwin(self.ma)
 
     def test_exists_looking(self):
         pass
@@ -29,9 +33,13 @@ class StoryTest(unittest.TestCase):
         m = self.ma
         data = [
                 ("chapter03", self.story, m.anri, m.yabu),
+                ("ep1", self.ep1, m.anri, m.yabu),
+                ("ep2", self.ep2, m.anri, m.yabu),
+                ("ep3", self.ep3, m.anri, m.yabu),
+                ("ep4", self.ep4, m.anri, m.yabu),
                 ]
 
-        for title, story, hero, rival in data[0:1]:
+        for title, story, hero, rival in data:
             with self.subTest(title=title, story=story, hero=hero, rival=rival):
                 self.assertTrue(testtools.has_basic_infos(self, story, hero, rival))
 
@@ -40,12 +48,37 @@ class StoryTest(unittest.TestCase):
         data = [
                 ("chapter03", self.story,
                     m.anri.have(m.race1, "優勝").want(),
-                    m.anri.have(m.grandticket).can(),
+                    m.anri.have(m.grandticket).must(),
                     m.anri.have(m.yabu, "協力"),
-                    m.anri.have("優勝を逃す")),
+                    m.anri.have(m.race1, "優勝").non(),
+                    ),
+                ("ep1", self.ep1,
+                    m.anri.know("何が起きたか").must(),
+                    m.anri.be("スタートできていない"),
+                    m.anri.remember("スタート"),
+                    m.anri.think(m.yabu, "嵌められた"),
+                    ),
+                ("ep2", self.ep2,
+                    m.anri.deal("遅れを取り戻す").must(),
+                    m.anri.be("出遅れた"),
+                    m.anri.do(m.angelwing, "使う"),
+                    m.anri.go(m.yabu, "追いつく"),
+                    ),
+                ("ep3", self.ep3,
+                    m.anri.go(m.race1, "ゴール"),
+                    m.anri.have(m.race1, "優勝").want(),
+                    m.anri.go("先に進む"),
+                    m.anri.be(m.taiga, "立ち塞がる"),
+                    ),
+                ("ep4", self.ep4,
+                    m.anri.have(m.race1, "優勝").want(),
+                    m.anri.have(m.grandticket).must(),
+                    m.anri.do(m.yabu, "協力", m.taiga, "倒す"),
+                    m.anri.have(m.race1, "優勝").non(),
+                    ),
                 ]
 
-        for title, story, what, why, how, result in data[0:1]:
+        for title, story, what, why, how, result in data:
             with self.subTest(title=title, story=story, what=what, why=why, how=how, result=result):
                 self.assertTrue(testtools.has_outline_infos(self, story, what, why, how, result, True))
 
