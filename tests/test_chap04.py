8912d961443d7c017264427b958f21f15d3cb959
N.T.WORKS <nagisc007@yahoo.co.jp>
2019-04-19 16:52:59 +0900

    [WIP] Added #8: outline and info test (#35)
    * [WIP] Added: outline and info test
    
    * Added: chapter 4 episode
    
    resolved #8

--- tests/test_chap04.py
+++ tests/test_chap04.py
@@ -6,7 +6,7 @@ import storybuilder.builder.testtools as testtools
 from storybuilder.builder.sbutils import print_test_title
 from src.main import master, story
 from src.main import CHARAS, STAGES
-from src.chapter04 import story04
+from src.chapter04 import story04, ep_intro, ep_boringday, ep_stranger, ep_retry
 
 
 _FILENAME = 'chapter04.py'
@@ -19,39 +19,72 @@ class StoryTest(unittest.TestCase):
         print_test_title(_FILENAME, "chapter 04")
 
     def setUp(self):
-        self.ma = master()
-        self.story = story04(self.ma)
+        self.m = master()
+        self.story = story04(self.m)
+        self.ep1 = ep_intro(self.m)
+        self.ep2 = ep_boringday(self.m)
+        self.ep3 = ep_stranger(self.m)
+        self.ep4 = ep_retry(self.m)
 
     def test_exists_looking(self):
         pass
 
     def test_has_basic_infos(self):
-        m = self.ma
+        m = self.m
         data = [
-                ("chapter04", self.story, m.anri, m.rondo),
+                ("chapter04", self.story, m.anri, m.sagisawa),
+                ("ep1", self.ep1, m.anri, m.chief),
+                ("ep2", self.ep2, m.anri, m.sagisawa),
+                ("ep3", self.ep3, m.anri, m.sagisawa),
+                ("ep4", self.ep4, m.anri, m.rondo),
                 ]
 
-        for title, story, hero, rival in data[0:1]:
+        for title, story, hero, rival in data:
             with self.subTest(title=title, story=story, hero=hero, rival=rival):
                 self.assertTrue(testtools.has_basic_infos(self, story, hero, rival))
 
     def test_has_outline_infos(self):
-        m = self.ma
+        m = self.m
         data = [
                 ("chapter04", self.story,
                     m.anri.have(m.ticket2).want(),
                     m.anri.have(m.grandticket).can(),
                     m.anri.hear(m.sagisawa, m.race2, m.sagi_proposal),
-                    m.anri.go(m.rondo, m.race2, "ペアで")),
+                    m.anri.go(m.rondo, m.race2, "ペアで"),
+                    ),
+                ("ep1", self.ep1,
+                    m.anri.look(m.race2, "参加方法").must(),
+                    m.anri.be("敗れる", m.race1),
+                    m.anri.have(m.dealer, m.race_ticket),
+                    m.chief.talk(m.anri, "諦めろ", m.race2),
+                    ),
+                ("ep2", self.ep2,
+                    m.anri.have(m.ticket2).must(),
+                    m.anri.go(m.race2, m.ticket2).can(),
+                    m.anri.look(m.some(), m.ticket2, "持っている"),
+                    m.anri.meet(m.sagisawa),
+                    ),
+                ("ep3", self.ep3,
+                    m.anri.hear(m.sagisawa),
+                    m.sagisawa.talk(m.anri, "レースに関する話"),
+                    m.anri.look("確かめる", m.sagi_proposal),
+                    m.sagisawa.talk(m.anri, m.sagi_proposal),
+                    ),
+                ("ep4", self.ep4,
+                    m.anri.deal("参加", m.race2).want(),
+                    m.anri.have(m.grandticket).must(),
+                    m.anri.deal("承諾", m.sagi_proposal),
+                    m.anri.go(m.rondo, m.race2, "ペアで"),
+                    ),
                 ]
 
-        for title, story, what, why, how, result in data[0:1]:
+        for title, story, what, why, how, result in data:
             with self.subTest(title=title, story=story, what=what, why=why, how=how, result=result):
                 self.assertTrue(testtools.has_outline_infos(self, story, what, why, how, result, True))
 
     @unittest.skip("in preparation")
     def test_has_the_action(self):
-        m = self.ma
+        m = self.m
         data = [
                 m.droid.explain("作業ドロイド"),
                 ]
@@ -62,7 +95,7 @@ class StoryTest(unittest.TestCase):
 
     @unittest.skip("in preparation")
     def test_has_the_keyword(self):
-        m = self.ma
+        m = self.m
         data = [
                 "ドロイド",
                 ]
