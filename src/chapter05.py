565bfa4b0a6fafe4b5a63f44e4a6bdea2fe3452f
N.T.WORKS <nagisc007@yahoo.co.jp>
2019-04-20 15:41:29 +0900

    [WIP] implement #9: episode test (empty) (#36)
    * [WIP] implement: episode test (empty)
    
    * [WIP] Added: episode test
    
    * Added: episodes
    
    resolved #9

--- src/chapter05.py
+++ src/chapter05.py
@@ -10,20 +10,87 @@ from storybuilder.builder.tools import build_to_story
 
 
 # episodes
+def ep_intro(m: Master):
+    racetime = m.race2day.elapsed_day(hour=13)
+    return m.story("ペアレース",
+            m.anri.be(m.factory, m.rondo, m.race2day),
+            m.rondo.talk(m.anri, "絶対に優勝").must(),
+            m.anri.reply(m.rondo, "そのつもり"),
+            m.anri.have(m.grandticket).want(),
+            m.anri.do(m.sagi_proposal, "約束を守る", m.grandticket).can(),
+            m.anri.go(m.race2stage, m.race2day, m.race_bus),
+            m.anri.think(m.rondo),
+            m.anri.deal("約束", m.sagi_proposal),
+            m.anri.ask(m.rondo, "なぜレースを？"),
+            m.rondo.reply(m.anri, m.rondo_reason),
+            m.rondo.talk(m.anri, m.kobucorp),
+            m.anri.know(m.rondo, m.kobucorp),
+            m.kobucorp.explain(m.dad, "父が代表の大企業"),
+            )
+
+
+def ep_promise(m: Master):
+    racetime = m.race2day.elapsed_day(hour=14)
+    waitroom = m.race2stage.insided("控室")
+    startpoint = m.race2stage.insided("スタート地点")
+    return m.story("暗黒の約束",
+            m.anri.go(m.rondo, m.race2stage, racetime),
+            m.anri.deal(m.rondo, m.race2, "受付"),
+            m.anri.deal(m.race2, "参加"),
+            m.anri.go(m.rondo, waitroom),
+            m.rondo.talk(m.anri, "あなたのことは聞いている"),
+            m.rondo.know(m.anri, m.dad),
+            m.rondo.talk(m.anri, m.rondo_promise),
+            m.anri.think("迷う", m.rondo_promise, m.sagi_proposal),
+            )
+
+
+def ep_rondo(m: Master):
+    racetime = m.race2day.elapsed_day(hour=15)
+    startpoint = m.race2stage.insided("スタート地点")
+    return m.story("レースの申し子ロンド",
+            m.anri.be(m.rondo, startpoint, racetime),
+            m.race2.explain("ペアレース"),
+            m.anri.go(m.rondo, "二人の合計タイム"),
+            m.anri.go(m.race2, m.rondo),
+            m.rondo.talk(m.anri, "足を引っ張らないでね"),
+            m.race2.do("開始"),
+            m.anri.be("備える"),
+            m.rondo.go("一人飛び出す"),
+            m.anri.go(m.rondo, "ついていく"),
+            m.rondo.be("圧倒的速度"),
+            m.rondo.go(m.anri, "置き去り"),
+            )
+
+
+def ep_obstacle(m: Master):
+    racetime = m.race2day.elapsed_day(hour=16)
+    return m.story("邪魔者",
+            m.anri.go(m.race2stage, racetime),
+            m.anri.go(m.rondo).must(),
+            m.anri.deal("約束を守る", m.sagi_proposal).must(),
+            m.anri.have(m.grandticket, m.sagi_proposal).can(),
+            m.anri.look(m.rondo),
+            m.rondo.be("恐ろしく速い"),
+            m.anri.deal("使う", m.angelwing),
+            m.anri.go("追いつく", m.rondo),
+            m.rondo.look(m.anri),
+            m.rondo.ask(m.anri, "そんなのでよくレーサーやってるね"),
+            m.anri.reply(m.rondo, m.anri_reason),
+            m.rondo.talk(m.anri_reason, "嫌いじゃない"),
+            m.anri.think(m.sagi_proposal, "悩む"),
+            m.anri.do(m.rondo, "邪魔"),
+            m.anri.talk(m.rondo, "失望").ps(),
+            )
+
 
 # main
-def story05(ma: Master):
-    return ma.story("Race 5",
-            ma.anri.go(ma.rondo, ma.race2),
-            ma.anri.have(ma.grandticket).want(),
-            ma.anri.do(ma.sagi_proposal, "約束を守る", ma.grandticket).can(),
-            ma.anri.go(ma.race2stage, ma.race2day),
-            ma.rondo.talk(ma.anri, "あなたのことは聞いている"),
-            ma.rondo.know(ma.anri, ma.dad),
-            ma.rondo.talk(ma.anri, ma.rondo_promise),
-            ma.anri.think("迷う", ma.rondo_promise, ma.sagi_proposal),
-            ma.anri.do(ma.rondo, "優勝を邪魔する"),
-            ma.anri.talk(ma.rondo, "失望").ps(),
+def story05(m: Master):
+    return m.story("Race 5",
+            ep_intro(m),
+            ep_promise(m),
+            ep_rondo(m),
+            ep_obstacle(m),
             )
 
 def main(): # pragma: no cover
--- tests/test_chap05.py
+++ tests/test_chap05.py
@@ -6,7 +6,7 @@ import storybuilder.builder.testtools as testtools
 from storybuilder.builder.sbutils import print_test_title
 from src.main import master, story
 from src.main import CHARAS, STAGES
-from src.chapter05 import story05
+from src.chapter05 import story05, ep_intro, ep_promise, ep_rondo, ep_obstacle
 
 
 _FILENAME = 'chapter05.py'
@@ -19,33 +19,66 @@ class StoryTest(unittest.TestCase):
         print_test_title(_FILENAME, "chapter 05")
 
     def setUp(self):
-        self.ma = master()
-        self.story = story05(self.ma)
+        self.m = master()
+        self.story = story05(self.m)
+        self.ep1 = ep_intro(self.m)
+        self.ep2 = ep_promise(self.m)
+        self.ep3 = ep_rondo(self.m)
+        self.ep4 = ep_obstacle(self.m)
 
     def test_exists_looking(self):
         pass
 
     def test_has_basic_infos(self):
-        m = self.ma
+        m = self.m
         data = [
                 ("chapter05", self.story, m.anri, m.rondo),
+                ("ep1", self.ep1, m.anri, m.rondo),
+                ("ep2", self.ep2, m.anri, m.rondo),
+                ("ep3", self.ep3, m.anri, m.rondo),
+                ("ep4", self.ep4, m.anri, m.rondo),
                 ]
 
-        for title, story, hero, rival in data[0:1]:
+        for title, story, hero, rival in data:
             with self.subTest(title=title, story=story, hero=hero, rival=rival):
                 self.assertTrue(testtools.has_basic_infos(self, story, hero, rival))
 
     def test_has_outline_infos(self):
-        m = self.ma
+        m = self.m
         data = [
                 ("chapter05", self.story,
                     m.anri.have(m.grandticket).want(),
                     m.anri.do(m.sagi_proposal, "約束を守る", m.grandticket).can(),
-                    m.anri.do(m.rondo, "優勝を邪魔する"),
-                    m.anri.talk(m.rondo, "失望").ps()),
+                    m.anri.do(m.rondo, "邪魔"),
+                    m.anri.talk(m.rondo, "失望").ps(),
+                    ),
+                ("ep1", self.ep1,
+                    m.anri.think(m.rondo),
+                    m.anri.deal("約束", m.sagi_proposal),
+                    m.anri.ask(m.rondo),
+                    m.anri.know(m.rondo, m.kobucorp),
+                    ),
+                ("ep2", self.ep2,
+                    m.anri.go(m.race2stage),
+                    m.anri.deal(m.race2, "参加"),
+                    m.anri.deal("受付", m.rondo),
+                    m.anri.go(m.rondo, m.race2stage),
+                    ),
+                ("ep3", self.ep3,
+                    m.anri.go(m.race2, m.rondo),
+                    m.race2.explain("ペアレース"),
+                    m.anri.go(m.rondo, "ついていく"),
+                    m.rondo.go(m.anri, "置き去り"),
+                    ),
+                ("ep4", self.ep4,
+                    m.anri.deal("約束を守る", m.sagi_proposal).must(),
+                    m.anri.have(m.grandticket, m.sagi_proposal).can(),
+                    m.anri.do("邪魔", m.rondo),
+                    m.anri.talk(m.rondo, "失望").ps(),
+                    ),
                 ]
 
-        for title, story, what, why, how, result in data[0:1]:
+        for title, story, what, why, how, result in data:
             with self.subTest(title=title, story=story, what=what, why=why, how=how, result=result):
                 self.assertTrue(testtools.has_outline_infos(self, story, what, why, how, result, True))
 
